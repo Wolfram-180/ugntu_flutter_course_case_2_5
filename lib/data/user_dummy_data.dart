1efb13ef69a9391cacd6429916512b119f103b7c
wolfram-180 <sergei.ulvis@gmail.com>
2021-12-27 17:12:30 +0300

    users list in ListView.builder - ListTile

---
+++ lib/data/user_dummy_data.dart
@@ -0,0 +1,7 @@
+import 'package:ugntu_flutter_course_case_2_5/model/user.dart';
+
+List<User> Users_list = [
+  User(name: 'Ivanov', phone: '111-222-333'),
+  User(name: 'Petrov', phone: '444-555-666'),
+  User(name: 'Sidorov', phone: '777-888-999'),
+];
